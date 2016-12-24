14b1e165bbfdc97e5e5a46177e39e7395472c9f7
krishnanGanesh <kg1682@nyu.edu>
2016-12-23 19:29:20 -0500

    BST with bfs included

---
+++ BinarySearchTree/Krishnan/BFS_BST/BFS_BST/Queue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace BFS_BST
+{
+   
+
+    public class Queue<T>
+    {
+
+        private int front;
+        private int rear;
+        int size;
+        T[] queue;
+
+        public Queue(int inSize)
+        {
+            size = inSize;
+            queue = new T[size];
+            front = -1;
+            rear = -1;
+        }
+
+        public bool isempty()
+        {
+            return (front == -1 && rear == -1);
+        }
+
+        public void enQueue(T value)
+        {
+            if ((rear + 1) % size == front)
+            {
+                Console.WriteLine("Queue is full");
+
+            }
+            else if (isempty())
+            {
+                front++;
+                rear++;
+                queue[rear] = value;
+
+            }
+            else
+            {
+                rear = (rear + 1) % size;
+                queue[rear] = value;
+
+            }
+        }
+
+        public T deQueue()
+        {
+            T value = default(T);
+            if (isempty())
+            {
+                Console.Write("Queue is empty, cant dequeue = ");
+            }
+            else if (front == rear)
+            {
+                value = queue[front];
+                front = -1;
+                rear = -1;
+
+            }
+            else
+            {
+                value = queue[front];
+                front = (front + 1) % size;
+
+            }
+            return value;
+
+        }
+    }
+}
